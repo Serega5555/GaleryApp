43bfa894ee330bd1a604ed7f981ea0e6b3c940ec
Serega5555 <lazarpalace@gmail.com>
2026-07-24 09:45:12 +0300

    Create ImageViewPage.xaml.cs

---
+++ GaleryApp/Pages/ImageViewPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using GaleryApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GaleryApp.Pages
+{
+    public partial class ImageViewPage : ContentPage
+    {
+        private ImageModel imageModel;
+        public ImageViewPage(ImageModel model)
+        {
+            InitializeComponent();
+            imageModel = model;
+            BindingContext = imageModel;
+            imageView.Source = imageModel.FilePath;
+            dateLabel.Text = $"Дата съемки: {imageModel.DateTaken}";
+        }
+    }
+}
