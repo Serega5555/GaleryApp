604ea9d5f0f1348b3f5b88e0797f5398b5542314
Serega5555 <lazarpalace@gmail.com>
2026-07-24 09:43:20 +0300

    Create ImageModel.cs

---
+++ GaleryApp/Models/ImageModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GaleryApp.Models
+{
+    public class ImageModel
+    {
+        public string FilePath { get; set; }
+        public string FileName { get; set; }
+        public string DateTaken { get; set; }
+    }
+}
